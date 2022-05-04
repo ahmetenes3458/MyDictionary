1c110cd33e9c95db30a3debcfa6b6b0d57c55ea8
Ahmet Enes Polat <ahmetenes3458@gmail.com>
2022-05-04 18:44:11 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> sehirler = new MyList<string>();
+            sehirler.Add("Sivas");
+            sehirler.Add("Ankara");
+            
+        }
+    }
+}
